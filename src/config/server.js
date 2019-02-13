c006ae1858b29759d17ef819d589ac430cd5bb08
davi873 <davi.davipires@gmail.com>
2019-02-13 18:36:28 -0200

    backend finalizado mas nao homologado

--- src/config/server.js
+++ src/config/server.js
@@ -2,10 +2,16 @@ const port = 3003
 
 const bodyParser = require('body-parser')
 const express = require('express')
+const queryParser = require('express-query-int')
 const server = express()
+
+const allowCors = require('../config/cors')
+
 
 server.use(bodyParser.urlencoded({ extended: true} ))
 server.use(bodyParser.json())
+server.use(queryParser)
+server.use(allowCors)
 
 server.listen(port, () => {
     console.log(`Backend is running on port ${port}!`)
